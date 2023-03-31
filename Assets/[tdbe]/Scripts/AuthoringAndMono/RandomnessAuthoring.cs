2d9b7b28ddf9b7bf66b5f3d65cac32a68543f854
tdbe <>
2023-03-31 14:25:55 +0200

    I initially designed my Randomness providers to be unique relative to each other and themselves. But I forgot to make them unique across different playthroughs of the game. So now I made an additional unix time based initial seed per game. Now, all games played in at least the last year, should be unique :)

--- Assets/[tdbe]/Scripts/AuthoringAndMono/RandomnessAuthoring.cs
+++ Assets/[tdbe]/Scripts/AuthoringAndMono/RandomnessAuthoring.cs
@@ -15,14 +15,18 @@ namespace GameWorld
         // Also "Nested native containers are illegal in jobs", but if you just send this native array
         // directly to the job, you get the bonus of being able to write back to it so you save
         // the randomness state.
-        
+        [Header("This is a relative offset, to make each \nrandomness system in the game unique.\nInternally there is another time based offset, \nto make each game unique.")]
         public uint randomSeed = 1;
 
         public class RandomnessBaker : Baker<RandomnessAuthoring>
         {
             public override void Bake(RandomnessAuthoring authoring)
             {
-                Unity.Mathematics.Random rs = Unity.Mathematics.Random.CreateFromIndex(authoring.randomSeed);
+                // TODO: theoretically since this is the global game uniqueness random seed, you might want it calculated in one place, not every time you author a randomness component..
+                long unixTimeMs = System.DateTimeOffset.Now.ToUnixTimeMilliseconds();
+                uint randSeed2 = (uint)(unixTimeMs % 100000000000);
+                
+                Unity.Mathematics.Random rs = Unity.Mathematics.Random.CreateFromIndex(authoring.randomSeed+randSeed2);
                 NativeArray<Unity.Mathematics.Random> rga = 
                     new NativeArray<Unity.Mathematics.Random>(
                         System.Environment.ProcessorCount*2, // I don't like having to guess the thread count
--- Assets/[tdbe]/Scripts/AuthoringAndMono/RandomnessSingleThreadedAuthoring.cs
+++ Assets/[tdbe]/Scripts/AuthoringAndMono/RandomnessSingleThreadedAuthoring.cs
@@ -8,14 +8,21 @@ namespace GameWorld
 {
     public class RandomnessSingleThreadedAuthoring : MonoBehaviour
     {
+        [Header("This is a relative offset, to make each \nrandomness system in the game unique.\nInternally there is another time based offset, \nto make each game unique.")]
         public uint randomSeed = 1;
 
         public class RandomnessSingleThreadedBaker : Baker<RandomnessSingleThreadedAuthoring>
         {
             public override void Bake(RandomnessSingleThreadedAuthoring authoring)
-            {
+            {   
+                // TODO: theoretically since this is the global game uniqueness random seed, you might want it calculated in one place, not every time you author a randomness component..
+                long unixTimeMs = System.DateTimeOffset.Now.ToUnixTimeMilliseconds();
+                uint randSeed2 = (uint)(unixTimeMs % 100000000000);
+                
                 AddComponent<RandomnessSingleThreadedComponent>(new RandomnessSingleThreadedComponent{
-                    randomGenerator = Unity.Mathematics.Random.CreateFromIndex(authoring.randomSeed)
+                    randomGenerator = Unity.Mathematics.Random.CreateFromIndex(
+                        authoring.randomSeed + randSeed2
+                    )
                 });
             }
         }
